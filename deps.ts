74d9a8decc4a4e36379af88c3836de388c167a2a
iFaxity <izze96@gmail.com>
2020-06-23 13:06:38 +0200

    format: code reformatted

--- deps.ts
+++ deps.ts
@@ -1,6 +1,4 @@
 // First party libraries
-export { HmacSha256 } from 'https://deno.land/std@v0.57.0/hash/sha256.ts';
-export { HmacSha512 } from 'https://deno.land/std@v0.57.0/hash/sha512.ts';
-export * as base64url from 'https://deno.land/std@v0.57.0/encoding/base64url.ts';
-//export { Sha3_224, Sha3_256, Sha3_384, Sha3_512 } from 'https://deno.land/std@v0.57.0/hash/sha3.ts';
-
+export { HmacSha256 } from "https://deno.land/std@v0.57.0/hash/sha256.ts";
+export { HmacSha512 } from "https://deno.land/std@v0.57.0/hash/sha512.ts";
+export * as base64url from "https://deno.land/std@v0.57.0/encoding/base64url.ts";
--- mod.ts
+++ mod.ts
@@ -4,59 +4,102 @@
  * Copyright(c) 2020 Christian Norrman
  * MIT Licensed
  */
-import { HmacSha256, HmacSha512, base64url } from './deps.ts';
+import { HmacSha256, HmacSha512, base64url } from "./deps.ts";
 
+/**
+ * Enum of supported algorithms for keygrip
+ * @enum
+ */
 export enum Algorithm {
-  SHA256 = 'sha256',
-  SHA512 = 'sha512',
+  SHA256 = "sha256",
+  SHA512 = "sha512",
 }
 
+/**
+ * Creates a class for storing private keys in a keychain to easily hash and verify data with different keys
+ * @class
+ */
 export class Keygrip {
-  #keys: string[];
-  #algo: Algorithm;
+  readonly #keys: string[];
+  readonly #algo: Algorithm;
 
-  constructor(keys: string[], algo: Algorithm=Algorithm.SHA256) {
+  /**
+   * @param {string[]} keys
+   * @param {Algorithm} [algo='sha256']
+   */
+  constructor(keys: string[], algo: Algorithm = Algorithm.SHA256) {
     if (!keys || !keys.length) {
-      throw new Error('Keys must be provided.');
-    } else if (algo != 'sha256' && algo != 'sha512') {
-      throw new Error('Algorithm not found');
+      throw new TypeError("Keys must be provided.");
+    } else if (algo != Algorithm.SHA256 && algo != Algorithm.SHA512) {
+      throw new TypeError("Invalid algorithm");
     }
 
     this.#keys = keys;
     this.#algo = algo;
   }
 
-  sign(data: string, key?: string): string {
-    let buffer: ArrayBuffer;
-    key = key ?? this.#keys[0];
+  /**
+   * Cryptographically sign data by a secret key
+   * @param {string} data Data to sign
+   * @param {string|number} [key] If key is a number it resolves the key as an index in the keychain.
+   *    If it is a string it is used directly. Defaults to first key in the keychain.
+   *
+   * @returns {string}
+   */
+  sign(data: string, key?: string | number): string {
+    const secret = (typeof key == "number" ? this.#keys[key] : key) ??
+      this.#keys[0];
+    let buf: ArrayBuffer;
 
-    if (this.#algo == 'sha256') {
-      const hash = new HmacSha256(key);
-      buffer = hash.update(data).arrayBuffer();
-    } else if (this.#algo == 'sha512') {
-      const hash = new HmacSha512(key);
-      buffer = hash.update(data).arrayBuffer();
-    } else {
-      throw new Error('Algorithm invalid');
+    switch (this.#algo) {
+      case Algorithm.SHA256:
+        buf = new HmacSha256(secret).update(data).arrayBuffer();
+        break;
+
+      case Algorithm.SHA512:
+        buf = new HmacSha512(secret).update(data).arrayBuffer();
+        break;
     }
 
-    return base64url.encode(buffer);
+    return base64url.encode(buf);
   }
 
+  /**
+   * Verifies if the data matches the digest with any of the keys in the keychain.
+   * @param {string} data Data to digest and verify
+   * @param {string} digest Digested hash to compare against
+   *
+   * @returns {boolean}
+   */
   verify(data: string, digest: string): boolean {
     return this.index(data, digest) != -1;
   }
 
+  /**
+   * Resolve the index of which key was used to digest specified data.
+   * @param {string} data Data to digest
+   * @param {string} digest Digested hash to compare against
+   *
+   * @returns {number}
+   */
   index(data: string, digest: string): number {
-    return this.#keys.findIndex(key => this.compare(digest, this.sign(data, key)));
+    return this.#keys.findIndex((key) =>
+      this.compare(digest, this.sign(data, key))
+    );
   }
 
-  // Timing safe compare using Brad Hill's Double HMAC pattern
+  /**
+   * Timing safe compare using Brad Hill's Double HMAC pattern
+   * @param {string} a Digested data a
+   * @param {string} b Digested data b
+   *
+   * @returns {boolean}
+   * @private
+   */
   private compare(a: string, b: string): boolean {
     const key = crypto.getRandomValues(new Uint8Array(32));
-    const hmac = new HmacSha256(key);
-    const ah = hmac.update(a).digest();
-    const bh = hmac.update(b).digest();
+    const ah = new HmacSha256(key).update(a).digest();
+    const bh = new HmacSha256(key).update(b).digest();
 
     return ah.length === bh.length && ah.every((x, i) => x === bh[i]);
   }
